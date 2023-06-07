64285464dd36d77c77c403bd54075a641338da55
Audrey Sestari Benetti <audrey.benetti@e-unicred.com.br>
2023-06-06 21:23:49 -0300

    Implementa interpolação para o ano no footer.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  year!: number;
+  constructor(){
+    this.year = new Date().getFullYear();
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
