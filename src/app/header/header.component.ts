c4d62c047f093838c9e5b7672aec5d11cef20e9b
Audrey Sestari Benetti <audrey.benetti@e-unicred.com.br>
2023-06-29 15:50:58 -0300

    Adiciona search bar no Header quando não estiver na Landing Page.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  searchBarHidden: boolean = true; 
+export class HeaderComponent implements OnInit {
+  searchBarHidden = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.searchBarHidden = this.router.url == '/';
+      }
+    });
+  }
 }
